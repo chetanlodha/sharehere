1811b099c0b061c17c1511eeb77e4cd14bea0d6b
luckytarun24 <luckyjaintj24@gmail.com>
2020-12-08 12:46:38 +0530

    complete

--- php/post/comments.php
+++ php/post/comments.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require_once 'conn.php';
+require_once '../conn.php';
 if(isset($_POST['comment']))
 {
-	if($_POST['comment']== 1)
+	if($_POST['comment']== 'create comment')
 	{
-		$comment_id = mysqli_real_escape_string($link, $_SESSION['user_id']);
+		$comment_id = mysqli_real_escape_string($link, $_SESSION['sess_id']);
 		$post_id = mysqli_real_escape_string($link, $_POST['post_id']);
 		$comment = mysqli_real_escape_string($link, $_POST['content']);
 		date_default_timezone_set("Asia/Calcutta");
@@ -13,7 +13,7 @@ if(isset($_POST['comment']))
 		$query = "INSERT INTO `comments` (`comment_id`, `post_id`,`comment`,`date_created`) VALUES ('$comment_id', '$post_id', '$comment','$date_now')";
 		if($result = mysqli_query($link, $query))
 		{  
-		    $data['name'] = $_SESSION['name']; 
+		    $data['name'] = $_SESSION['user_name']; 
 		    $data['content'] = $comment;
 			$data['date_created'] = $date_now;
 			$data['status'] = 201;
@@ -26,7 +26,7 @@ if(isset($_POST['comment']))
 			echo json_encode($data);
 		}
 	}
-	else if($_POST['comment']== 2)
+	else if($_POST['comment']== 'fetch comment')
 	{
 	    $post_id = mysqli_real_escape_string($link, $_POST['post_id']);	
 		$query = "SELECT * FROM `comments` WHERE `post_id` = '$post_id'";
@@ -36,9 +36,9 @@ if(isset($_POST['comment']))
 		    $data = array(array());
 		    while($row = $result->fetch_assoc())
 			{
-				$data[$i]['user_id'] = $row['comment_id'];
-				$data[$i]['comments'] = $row['comment'];
-				$data[$i]['date_create'] = $row['date_created'];
+				$data[$i]['id'] = $row['comment_id'];
+				$data[$i]['comments'] = $row['content'];
+				$data[$i]['date_created'] = $row['date_created'];
 				$i++;
 		    }
 			echo json_encode($data);
@@ -51,11 +51,11 @@ if(isset($_POST['comment']))
 			echo json_encode($data);
 		}
 	}
-    else 
+    else if($_POST['comment']== 'delete comment') 
     {
-		$comment_id = mysqli_real_escape_string($link, $_SESSION['user_id']);
+		$comment_id = mysqli_real_escape_string($link, $_SESSION['sess_id']);
 		$post_id = mysqli_real_escape_string($link, $_POST['post_id']);
-		$date = mysqli_real_escape_string($link, $_POST['time']);
+		$date = mysqli_real_escape_string($link, $_POST['date_created']);
 		$query = "DELETE FROM `comments` WHERE `comment_id` = '$comment_id' AND `post_id` = '$post_id' AND`date_created` = '$date'";
 		if($result = mysqli_query($link, $query))
 		{  
@@ -69,7 +69,34 @@ if(isset($_POST['comment']))
 			echo json_encode($data);
 		}
 		
+	}
+	 else if($_POST['comment']== 'update comment') 
+    {
+		$comment_id = mysqli_real_escape_string($link, $_SESSION['sess_id']);
+		$post_id = mysqli_real_escape_string($link, $_POST['post_id']);
+		$comment = mysqli_real_escape_string($link, $_POST['content']);
+		$date = mysqli_real_escape_string($link, $_POST['date_created']);
+		date_default_timezone_set("Asia/Calcutta");
+		$date_now = date("r");
+	    $query = "UPDATE `comments` SET `date_created` = '$date_now' , `comment` = '$comment' WHERE `comment_id` = '$comment_id' AND `post_id` = '$post_id' AND`date_created` = '$date'";	
+	    if($result = mysqli_query($link, $query))
+		{  
+		    $data['content'] = $comment;
+			$data['date_created'] = $date_now;
+			$data['status'] = 201;
+			echo json_encode($data);
+		}  
+		else  
+		{  
+			$data['status'] = 601;
+			$data['error'] = $link -> error;
+			echo json_encode($data);
+		}
 	}	
+    else
+    {
+	    header('Location: http://index.php');
+    } 	
 }
 else
 {
--- php/post/create_post.php
+++ php/post/create_post.php
@@ -1,79 +1,128 @@
 <?php
 session_start();
 require_once '../conn.php';
-$data = array();
-if($_FILES['image'] || isset($_POST['content'])){
-    $post_id = mysqli_real_escape_string($link, md5(uniqid()));
-	$user_id = mysqli_real_escape_string($link, $_SESSION['sess_id']);
-	date_default_timezone_set("Asia/Calcutta");
-    $date_now = date("r");
-	$date_created = $date_now;
-	$last_updated = $date_now;
-	$countfiles = count($_FILES['image']['name']);
-	if(!empty($_FILES['image']['name'][0]))
+if($_POST['post'] == 'create post')
+{
+	$data = array();
+	if($_FILES['image'] || isset($_POST['content']))
 	{
-		$file = array();
-		$maxsize = 5242880;
-		if((array_sum($_FILES['image']['size']) >= $maxsize) || ($_FILES["image"]["size"] == 0)) {
-            $file['status'] = 601;
-		    $file['error'] = "File too large. File must be less than 5MB.";
-        }
-		else{
-			$path = getcwd(); 
-			for($i=1;$i<=$countfiles;$i++){
-				$img = $_FILES['image']['name'][$i-1];
-				$tmp = $_FILES['image']['tmp_name'][$i-1];
-				$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-				$final_image = $post_id."_".$i.'.'.$ext;
-				$file[$i] = $final_image;
-				move_uploaded_file($tmp,"$path/post/$final_image"); 
-				
+		$post_id = mysqli_real_escape_string($link, md5(uniqid()));
+		$user_id = mysqli_real_escape_string($link, $_SESSION['sess_id']);
+		date_default_timezone_set("Asia/Calcutta");
+		$date_now = date("r");
+		$date_created = $date_now;
+		$last_updated = $date_now;
+		$countfiles = count($_FILES['image']['name']);
+		if(!empty($_FILES['image']['name'][0]))
+		{
+			$file = array();
+			$maxsize = 5242880;
+			if((array_sum($_FILES['image']['size']) >= $maxsize) || ($_FILES["image"]["size"] == 0)) {
+				$file['status'] = 601;
+				$file['error'] = "File too large. File must be less than 5MB.";
 			}
+			else{
+				$path = getcwd(); 
+				for($i=1;$i<=$countfiles;$i++){
+					$img = $_FILES['image']['name'][$i-1];
+					$tmp = $_FILES['image']['tmp_name'][$i-1];
+					$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+					$final_image = $post_id."_".$i.'.'.$ext;
+					$file[$i] = $final_image;
+					move_uploaded_file($tmp,"$path/post/$final_image"); 
+					
+				}
+			}	
+		}
+		else
+		{
+		   $file = NULL;
+		}
+		if(isset($_POST['content']))
+		{
+		   $content = $_POST['content'];  
+		}
+		else
+		{
+		   $content =  NULL;
+		}
+		$comments = 0;
+		$likes = 0;
+		date_default_timezone_set("Asia/Calcutta");
+		$date_now = date("r");
+		$last_updated = $date_now;
+		$query = "INSERT INTO `post` (`post_id`, `user_id`, `media`,`content`,`date_created`,`last_updated`, `comments`, `likes`) VALUES ('$post_id', '$user_id', '$countfiles','$content','$date_created','$last_updated', '$comments','$likes')";
+		if(!isset($data['status']))
+		{
+			if($result = mysqli_query($link, $query))
+			{  
+			    $data['post_id'] = $post_id;
+				$data['likes'] = $likes;
+				$data['comments'] = $comments;
+				$data['media'] = $file;
+				$data['last_updated'] = $last_updated;
+				$data['content'] = $content;
+				$data['status'] = 201;
+				echo json_encode($data);
+			}		
+			else  
+			{  
+				$data['status'] = 601;
+				$data['error'] = $link -> error;
+				echo json_encode($data);
+			}
+		}
+		else
+		{
+			echo json_encode($data);
+
 		}	
+    }
+	else  
+	{  
+		$data['status'] = 601;
+		$data['error'] = $link -> error;
+		echo json_encode($data);
 	}
-	else
-	{
-	   $file = NULL;
+}
+else if($_POST['post'] == 'delete post' && isset($_SESSION['post_id']) )
+{
+	$post_id = mysqli_real_escape_string($link, $_SESSION['post_id']);
+	$query = "DELETE FROM `post` WHERE `post_id` = '$post_id'";
+	if($result = mysqli_query($link, $query))
+	{  
+		$data['status'] = 201;
+		echo json_encode($data);
+	}  
+	else  
+	{  
+		$data['status'] = 601;
+		$data['error'] = $link -> error;
+		echo json_encode($data);
 	}
-	if(isset($_POST['content']))
-	{
-	   $content = $_POST['content'];  
-	}
-	else
-	{
-	   $content =  NULL;
- 	}
-	$comments = 0;
-	$likes = 0;
-    date_default_timezone_set("Asia/Calcutta");
+}
+else if($_POST['post'] == 'update post' && isset($_SESSION['post_id']))
+{
+   
+	$post_id = mysqli_real_escape_string($link, $_SESSION['post_id']);
+	$content = mysqli_real_escape_string($link, $_SESSION['content'])
+	date_default_timezone_set("Asia/Calcutta");
 	$date_now = date("r");
 	$last_updated = $date_now;
-    $query = "INSERT INTO `post` (`post_id`, `user_id`, `media`,`content`,`date_created`,`last_updated`, `comments`, `likes`) VALUES ('$post_id', '$user_id', '$countfiles','$content','$date_created','$last_updated', '$comments','$likes')";
-    if(!isset($data['status']))
-	{
-    	if($result = mysqli_query($link, $query))
-    	{  
-		    $data['likes'] = $likes;
-			$data['comments'] = $comments;
-			$data['media'] = $file;
-	    	$data['last_updated'] = $last_updated;
-			$data['content'] = $content;
-			$data['status'] = 201;
-     		echo json_encode($data);
-	    }		
-		else  
-		{  
-			$data['status'] = 601;
-			$data['error'] = $link -> error;
-			echo json_encode($data);
-		}
-	}
-    else
-	{
+	$query = "UPDATE `post` SET `content` = '$content',`last_updated` = '$last_updated' WHERE `post_id` = '$post_id'";	
+	if($result = mysqli_query($link, $query))
+	{  
+	    $data['content'] = $content;
+		$data['last_updated'] = $last_updated; 
+		$data['status'] = 201;
 		echo json_encode($data);
-
-	}	
-    
+	}  
+	else  
+	{  
+		$data['status'] = 601;
+		$data['error'] = $link -> error;
+		echo json_encode($data);
+	}
 }
 else
 {
